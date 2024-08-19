f8d2610b60545f26646fdf3611ec355fba8c8bc7
MayCleanSitepu <113108027+MayCleanSitepu@users.noreply.github.com>
2024-08-19 16:53:04 +0700

    [add] 3D model of IPhone with color change

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,28 +1,48 @@
-import { Html, PerspectiveCamera, View } from "@react-three/drei"
-import Lights from "./Lights"
-import Iphone from "./Iphone"
-import { Suspense } from "react"
+import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
 
-const ModelView = ( index, groupRef, gsapType, controlRef, setRotationSize, size, item) => {
+import * as THREE from 'three'
+import Lights from './Lights';
+import IPhone from './Iphone';
+import { Suspense } from "react";
+
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
   return (
     <View
-    index={index}
-    id={gsapType}
-    className={`border-2 border-red-500 w-full h-full
-      ${index === 2 }? 'right-[-100%] :''`}
+      index={index}
+      id={gsapType}
+      className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
     >
-
+      {/* Ambient Light */}
       <ambientLight intensity={0.3} />
-      <PerspectiveCamera makeDefault position={[0,0,4]}/>
-      <Lights/>
-      <Suspense fallback={
-        <Html>
-          <div>Loading</div>
-        </Html>
-        }>
-        <Iphone/>
-      </Suspense>
 
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+
+      <OrbitControls 
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0 ,0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      /> 
+
+      <group ref={groupRef} name={`${index === 1} ? 'small' : 'large`} position={[0, 0 ,0]}>
+        <Suspense fallback={
+          <Html>
+            <div className="w-full h-full flex items-center justify-center">
+              <p>Loading...</p>
+            </div>
+          </Html>}>
+          <IPhone 
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
     </View>
   )
 }
